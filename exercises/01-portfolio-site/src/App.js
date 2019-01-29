6fda8964a27036aa7fe2b7c84faa96b21bfa268e
kevinyu05 <44944091+kevinyu05@users.noreply.github.com>
2019-01-28 16:19:33 -0800

    updated not found page

--- exercises/01-portfolio-site/src/App.js
+++ exercises/01-portfolio-site/src/App.js
@@ -9,6 +9,7 @@ import {
 
 import About from './pages/About'
 import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
 import Project from './pages/Project'
 import Skills from './pages/Skills'
 import Home from './pages/Home'
